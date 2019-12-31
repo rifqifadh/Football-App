108901a50e8bb0983eb2cb0397009217fbf1b491
telkomdev-rifqi <rifqi.fadhlillah@gmail.com>
2019-12-31 23:22:47 +0700

    fixing

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -28,7 +28,7 @@ send_msg "
 Travis build *${build_status}!*
 \`Repository:  ${TRAVIS_REPO_SLUG}\`
 \`Branch:      ${TRAVIS_BRANCH}\`
-\`Files:       [Download Apk](https://github.com/rifqifadh/Football-App/raw/master/app/release/app-release.apk)\`
+\`Files:       [Download Apk](http://www.example.com)\`
 *Commit Msg:*
 ${TRAVIS_COMMIT_MESSAGE}
 [Job Log here](${TRAVIS_JOB_WEB_URL})
